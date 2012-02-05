56c3ab933182f338a43b4b4e9a68458f2441d5bf
Will Abson <will.abson@gmail.com>
2012-02-05 17:22:46 +0000

    Add audio previews to 2.x Media Previews add-on. Name of previewer to be used in config is 'MP3Player', supports MP3 files only at the moment.
    git-svn-id: https://share-extras.googlecode.com/svn/trunk/Media Preview@932 a3f5c567-fd0f-3a89-9b71-a290c5a5f590

--- source/web/extras/components/preview/MP3Player.js
+++ source/web/extras/components/preview/MP3Player.js
@@ -17,23 +17,23 @@
  */
 
 /**
- * This is the "FLVPlayer" plug-in used to display documents directly in the web browser.
+ * This is the "MP3Player" plug-in used to play audio files in the web browser.
  *
- * It supports the "video/x-flv" and "video/mp4" mime types directly, and other video types
- * that can be converted into these formats (e.g. via FFmpeg transformations).
+ * It supports the "audio/mp3" mime type directly, and other audio types
+ * that can be converted into this formats (e.g. via FFmpeg transformations).
  *
- * TODO Use the same video thumbnail renditions as the base previewers?
+ * TODO Use the same audio thumbnail renditions as the base previewers?
  *
  * @namespace Alfresco.WebPreview.prototype.Plugins
- * @class Alfresco.WebPreview.prototype.Plugins.FLVPlayer
+ * @class Alfresco.WebPreview.prototype.Plugins.MP3Player
  * @author Will Abson
  */
 (function()
 {
     /**
-     * Flash video mimetype string
+     * MP3 audio mimetype string
      */
-    const MIMETYPE_FLV = "video/x-flv";
+    const MIMETYPE_MP3 = "audio/mpeg";
     
     /**
      * H264 video mimetype string
@@ -41,14 +41,14 @@
     const MIMETYPE_H264 = "video/mp4";
     
     /**
-     * FLVPlayer plug-in constructor
+     * MP3Player plug-in constructor
      * 
-     * TODO Make this generic enough to handle the audio previews as well via a parameter e.g. <plugin previewer="flvplayer" ... />?
+     * TODO Make this generic enough to handle the audio previews as well via a parameter e.g. <plugin previewer="MP3Player" ... />?
      *
      * @param wp {Alfresco.WebPreview} The Alfresco.WebPreview instance that decides which plugin to use
      * @param attributes {Object} Arbitrary attributes brought in from the <plugin> element
      */
-    Alfresco.WebPreview.prototype.Plugins.FLVPlayer = function(wp, attributes)
+    Alfresco.WebPreview.prototype.Plugins.MP3Player = function(wp, attributes)
     {
        /* Decoupled event listeners are added in setOptions */
        YAHOO.Bubbling.on("documentDetailsAvailable", this.onDocumentDetailsAvailable, this);
@@ -63,7 +63,7 @@
        return this;
     };
     
-    Alfresco.WebPreview.prototype.Plugins.FLVPlayer.prototype =
+    Alfresco.WebPreview.prototype.Plugins.MP3Player.prototype =
     {
        /**
         * Attributes
@@ -101,16 +101,16 @@
           disableI18nInputFix: "false",
 
           /**
-           * Name of the thumbnail which, if present, is used to display a static image representing the video
+           * Name of the thumbnail which, if present, is used to display a static image representing the audio
            * before the play button is pressed. Use 'imgpreview' for the scaled-down thumbnail provided by the
            * OOTB config, or 'imgpreviewfull' for the full-size version (config provided by this add-on)
            */
           posterThumbnailName: "imgpreviewfull",
 
           /**
-           * Name of the thumbnail which, if present, contains a Flash video rendition of the video
+           * Name of the thumbnail which, if present, contains a MP3 audio rendition of the file
            */
-          flvThumbnailName: "flvpreview",
+          mp3ThumbnailName: "mp3preview",
           
           /**
            * Name of the thumbnail which, if present, contains a H264 rendition of the video
@@ -118,7 +118,7 @@
           h264ThumbnailName: "h264preview",
           
           /**
-           * Whether to queue missing video renditions
+           * Whether to queue missing audio renditions
            */
           queueMissingRenditions: true
        },
@@ -131,9 +131,9 @@
         *         it cant be used as a string.
         * @public
         */
-       report: function FLVPlayer_report()
+       report: function MP3Player_report()
        {
-           return this._getSupportedVideoMimeTypes().length > 0 ? "" : "No supported MIME type available";
+           return this._getSupportedAudioMimeTypes().length > 0 ? "" : "No supported MIME type available";
        },
     
        /**
@@ -142,13 +142,14 @@
         * @method display
         * @public
         */
-       display: function FLVPlayer_display()
+       display: function MP3Player_display()
        {
           var previewCtx = this.resolveUrls();
           
-          if (previewCtx.videourl) // Present if video is a natively-previewable type, e.g. flv or mp4
+          if (previewCtx.audiourl) // Present if audio is a natively-previewable type, e.g. mp3
           {
               this._displayPlayer(previewCtx);
+              return "<div></div>";
           }
           else // Otherwise we need to work out which renditions have already been generated
           {
@@ -178,23 +179,24 @@
                         *  }
                        */
                        
-                       if (previewCtx.videourl) // Present if video is a native mimetype, e.g. mp4
+                       if (previewCtx.audiourl) // Present if audio is a native mimetype, e.g. mp3
                        {
                            this._displayPlayer(previewCtx);
+                           return "<div></div>";
                        }
                        else
                        {
-                          // Video rendition is not yet ready, or could not be generated
+                          // Audio rendition is not yet ready, or could not be generated
                           if (this.attributes.queueMissingRenditions)
                           {
                               // Fire off a request to queue the rendition generation, if it's not already been done
                               if (!this.thumbnailQueued)
                               {
-                                  this._queueVideoThumbnailGeneration();
+                                  this._queueAudioThumbnailGeneration();
                                   this.thumbnailQueued = true;
                               }
                               
-                              // Add a message to the preview area to indicate we are waiting for the video to be converted
+                              // Add a message to the preview area to indicate we are waiting for the audio to be converted
                               var pEl = this.wp.getPreviewerElement();
                               pEl.innerHTML = "";
                               var msgEl = document.createElement("div");
@@ -203,7 +205,7 @@
                               //msgEl.set("id", this.wp.id + "-full-window-div");
                               //msgEl.setStyle("position", "absolute");
                               Dom.addClass(msgEl, "message");
-                              msgEl.innerHTML = this.wp.msg("label.videoConverting");
+                              msgEl.innerHTML = this.wp.msg("label.audioConverting");
                               pEl.appendChild(msgEl);
                               
                               // Poll again in 10 seconds, to see if the thumbnail is available yet
@@ -222,10 +224,10 @@
 
                               // TODO Add poster behind this message area, if available
                               var msg = '';
-                              msg += this.wp.msg("label.noVideoAvailable");
+                              msg += this.wp.msg("label.noAudioAvailable");
                               msg += '<br/>';
                               msg += '<a class="theme-color-1" href="' + this.wp.getContentUrl(true) + '">';
-                              msg += this.wp.msg("label.noVideoDownloadFile");
+                              msg += this.wp.msg("label.noAudioDownloadFile");
                               msg += '</a>';
 
                               msgEl.innerHTML = msg;
@@ -243,12 +245,12 @@
        },
     
        /**
-        * Display the Flash video player
+        * Display the Flash audio player
         *
         * @method _displayPlayer
         * @private
         */
-       _displayPlayer: function FLVPlayer_display(previewCtx)
+       _displayPlayer: function MP3Player_display(previewCtx)
        {
            // To support "full window" we create a new div that will float above the rest of the ui
            this.createSwfDiv();
@@ -256,24 +258,19 @@
            // TODO Use the poster image (if present) dimensions to set the dimensions of the movie player
 
            // Create flash web preview by using swfobject
-           var swfId = "VideoPreviewer_" + this.wp.id,
+           var swfId = "AudioPreviewer_" + this.wp.id,
                argsNoCache = "?noCacheToken=" + new Date().getTime();
-           var so = new YAHOO.deconcept.SWFObject(Alfresco.constants.URL_CONTEXT + "res/extras/components/preview/player_flv_maxi.swf" + argsNoCache,
+           var so = new YAHOO.deconcept.SWFObject(Alfresco.constants.URL_CONTEXT + "res/extras/components/preview/player_mp3_maxi.swf" + argsNoCache,
                  swfId, "100%", "100%", "6.0.0");
            so.addVariable("fileName", this.wp.options.name);
-           so.addVariable("flv", previewCtx.videourl);
-           if (previewCtx.imageurl != null)
-           {
-              so.addVariable("startimage", previewCtx.imageurl);
-           }
-           so.addVariable("showfullscreen", 1);
-           so.addVariable("showiconplay", 1);
-           so.addVariable("showplayer", "always");
+           so.addVariable("mp3", previewCtx.audiourl);
+           so.addVariable("title", this.wp.msg("preview.preparingAudio"));
            so.addVariable("showvolume", 1);
-           so.addVariable("showtime", 1);
-           so.addVariable("playercolor", "e1e3e5");
-           so.addVariable("margin", "0");
-           so.addVariable("buttoncolor", "000000");
+           so.addVariable("bgcolor1", "e1e3e5");
+           so.addVariable("bgcolor2", "999999");
+           so.addVariable("slidercolor1", "999999");
+           so.addVariable("slidercolor2", "666666");
+           so.addVariable("buttoncolor", "333333");
            so.addVariable("buttonovercolor", "0088de");
            so.addVariable("sliderovercolor", "0088de");
            so.addParam("allowScriptAccess", "sameDomain");
@@ -340,7 +337,7 @@
        },
 
        /**
-        * Because the VideoPreview content is absolutely positioned, components which alter DOM layout can fire
+        * Because the preview content is absolutely positioned, components which alter DOM layout can fire
         * this event to prompt a recalculation of the absolute coordinates.
         *
         * @method onRecalculatePreviewLayout
@@ -357,75 +354,72 @@
        },
        
        /**
-        * Return mime types supported by the video previewer for this file
+        * Return mime types supported by the audio previewer for this file
         * 
-        * @method _getSupportedVideoMimeTypes
-        * @return Array containing the video MIME types supported by the Flash video player for this file, in order of preference
+        * @method _getSupportedAudioMimeTypes
+        * @return Array containing the audio MIME types supported by the Flash audio player for this file, in order of preference
         */
-       _getSupportedVideoMimeTypes: function VP__getSupportedVideoMimeTypes()
+       _getSupportedAudioMimeTypes: function VP__getSupportedAudioMimeTypes()
        {
            var ps = this.previews, // List of thumbnails that are available (might not have actually been generated yet) 
-               flvpreview = this.attributes.flvThumbnailName, h264preview = this.attributes.h264ThumbnailName, 
-               flvmimetype = MIMETYPE_FLV, h264mimetype = MIMETYPE_H264,
+               mp3preview = this.attributes.mp3ThumbnailName, 
+               mp3mimetype = MIMETYPE_MP3,
                mimetype = this.wp.options.mimeType,
                mimetypes = [];
           
-          // Require Flash player 9.0.115 (9 beta 3) for h264 video
-          if ((Alfresco.util.arrayContains(ps, h264preview) || mimetype == h264mimetype) && Alfresco.util.hasRequiredFlashPlayer(9, 0, 115))
+          // Require Flash player 9.0.45 for MP3 audio
+          // TODO verify this version is required
+          if ((Alfresco.util.arrayContains(ps, mp3preview) || mimetype == mp3mimetype) && Alfresco.util.hasRequiredFlashPlayer(9, 0, 45))
           {
-             mimetypes.push(h264mimetype);
-          }
-          if ((Alfresco.util.arrayContains(ps, flvpreview) || mimetype == flvmimetype) && Alfresco.util.hasRequiredFlashPlayer(6, 0, 0))
-          {
-             mimetypes.push(flvmimetype);
+             mimetypes.push(mp3mimetype);
           }
           return mimetypes;
        },
 
        /**
-        * Helper method for deciding what previews to use to display the video content plus a still image from the video
+        * Helper method for deciding what previews to use to display the audio content plus a still image from the audio
         *
         * @method resolveUrls
-        * @return An object with two properties - 'videourl' contains the video content URL to use, 'imageurl' contains the still image URL. Either or both properties may be null if no appropriate thumbnail definitions can be found
+        * @return An object with two properties - 'audiourl' contains the audio content URL to use, 'imageurl' contains the still image URL. Either or both properties may be null if no appropriate thumbnail definitions can be found
         */
        resolveUrls: function VP_resolveUrls()
        {
-          var ps = this.previews, videopreview,
+          var ps = this.previews, audiopreview,
              psa = this.availablePreviews, 
-             flvpreview = this.attributes.flvThumbnailName, h264preview = this.attributes.h264ThumbnailName,
+             mp3preview = this.attributes.mp3ThumbnailName,
              imgpreview = this.attributes.posterThumbnailName,
-             videourl, imageurl;
+             audiourl, imageurl;
 
           // Static image to display before the user clicks 'play' - we do not care if this has been generated already or not
           imageurl = Alfresco.util.arrayContains(ps, imgpreview) ? this.wp.getThumbnailUrl(imgpreview) : null;
           
-          var supportedtypes = this._getSupportedVideoMimeTypes();
+          var supportedtypes = this._getSupportedAudioMimeTypes();
           
           // Permissively allow the content item itself to be returned if supported - strict compliance would imply we always return the preferred thumbnail format
           if (Alfresco.util.arrayContains(supportedtypes, this.wp.options.mimeType))
           {
              /* The content matches an image mimetype that the player can handle without a preview */
-             videourl = this.wp.getContentUrl();
+             audiourl = this.wp.getContentUrl();
              return (
              {
-                videourl: videourl,
+                audiourl: audiourl,
                 imageurl: imageurl
              });
           }
           else
           {
              // Always use the preferred thumbnail format, even if less-preferred formats are available and have been generated already
-             videopreview = supportedtypes.length > 0 ? (supportedtypes[0] == MIMETYPE_H264 ? h264preview : (supportedtypes[0] == MIMETYPE_FLV ? flvpreview : null)) : null;
+              audiopreview = supportedtypes.length > 0 ? (supportedtypes[0] == MIMETYPE_MP3 ? mp3preview : null) : null;
              
-             if (videopreview !== null) // Can the content can be previewed?
+             if (audiopreview !== null) // Can the content can be previewed?
              {
-                if (this.availablePreviews != null && Alfresco.util.arrayContains(psa, videopreview)) // Is a video preview available (i.e already generated)?
+                if (this.availablePreviews != null && Alfresco.util.arrayContains(psa, audiopreview)) // Is a audio preview available (i.e already generated)?
                 {
-                   videourl = this.wp.getThumbnailUrl(videopreview);
+                   audiourl = this.wp.getThumbnailUrl(audiopreview);
                 }
                 return (
                 {
-                   videourl: videourl,
+                   audiourl: audiourl,
                    imageurl: imageurl
                 });
              }
@@ -437,25 +431,25 @@
        },
        
        /**
-        * Fire off a request to the repository to queue the creation of video renditions. This will return a 404 if the queue request
+        * Fire off a request to the repository to queue the creation of audio renditions. This will return a 404 if the queue request
         * completes successfully, or a 500 if an error occurs
         * 
-        * @method _queueVideoThumbnailGeneration
+        * @method _queueAudioThumbnailGeneration
         * @return
         */
-       _queueVideoThumbnailGeneration: function VP_queueVideoThumbnailGeneration ()
+       _queueAudioThumbnailGeneration: function VP_queueAudioThumbnailGeneration ()
        {
-          var ps = this.previews, videopreview,
-          flvpreview = this.attributes.flvThumbnailName, h264preview = this.attributes.h264ThumbnailName;
+          var ps = this.previews, audiopreview,
+             mp3preview = this.attributes.mp3ThumbnailName;
           
-          videopreview = Alfresco.util.arrayContains(ps, h264preview) ? h264preview : (Alfresco.util.arrayContains(ps, flvpreview) ? flvpreview : null);
+          audiopreview = Alfresco.util.arrayContains(ps, mp3preview) ? mp3preview : null;
           
-          if (videopreview !== null)
+          if (audiopreview !== null)
           {
              var actionUrl = YAHOO.lang.substitute(Alfresco.constants.PROXY_URI + "api/node/{nodeRef}/content/thumbnails/{thumbnailname}?c=queue",
              {
                 nodeRef: this.wp.options.nodeRef.replace(":/", ""),
-                thumbnailname: videopreview
+                thumbnailname: audiopreview
              });
              
              Alfresco.util.Ajax.request(
@@ -464,14 +458,14 @@
                 url: actionUrl,
                 successCallback:
                 {
-                   fn: function VP_onQueueVideoThumbnailSuccess(event, obj)
+                   fn: function VP_onQueueAudioThumbnailSuccess(event, obj)
                    {
                    },
                    scope: this
                 },
                 failureCallback:
                 {
-                   fn: function VP_onQueueVideoThumbnailFailure(event, obj)
+                   fn: function VP_onQueueAudioThumbnailFailure(event, obj)
                    {
                        // TODO Display an error dialog (and log?) that the thumbnail generation could not be queued, if we get a non-404 response code
                    },
