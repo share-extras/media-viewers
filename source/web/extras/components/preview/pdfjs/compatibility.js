0aea31464ec445c17db2826661dc0e9bab62465a
Peter Lofgren <peter@loftux.se>
2013-06-12 15:30:04 +0200

    Updated to PDFJS.version = '0.8.237' - PDFJS.build = '152e376'

--- source/web/extras/components/preview/pdfjs/compatibility.js
+++ source/web/extras/components/preview/pdfjs/compatibility.js
@@ -20,7 +20,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* globals VBArray */
+/* globals VBArray, PDFJS */
 
 'use strict';
 
@@ -443,4 +443,19 @@
       },
       enumerable: true
    });
+})();
+
+(function checkRangeRequests() {
+   // Safari has issues with cached range requests see:
+   // https://github.com/mozilla/pdf.js/issues/3260
+   // Last tested with version 6.0.4.
+   var isSafari = Object.prototype.toString.call(
+      window.HTMLElement).indexOf('Constructor') > 0;
+   if (!isSafari) {
+      return;
+   }
+   document.addEventListener('DOMContentLoaded', function (e) {
+      console.warn('Range requests are disabled for safari.');
+      PDFJS.disableRange = true;
+   }, true);
 })();
