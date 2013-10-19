a873228b5b305c717d6c6bdeb1fa21f869c5002f
Peter Lofgren <peter@loftux.se>
2013-10-19 18:26:05 +0200

    #22 Filmstrip view - Dependencies (css and js) loaded when enabling viewer using module configuration.

---
+++ config/webscripts/org/sharextras/customization/media-previews/flvplayer-viewrender/documentlist-v2.get.js
@@ -0,0 +1,4 @@
+// Add dependencies for documentlibrary view renders.
+model.viewCssDeps.push("extras/components/preview/FLVPlayer.css");
+
+model.viewJsDeps.push("extras/components/preview/FLVPlayer.js");
---
+++ config/webscripts/org/sharextras/customization/media-previews/prettify-viewrender/documentlist-v2.get.js
@@ -0,0 +1,7 @@
+// Add dependencies for documentlibrary view renders.
+model.viewCssDeps.push("extras/components/preview/Prettify.css");
+model.viewCssDeps.push("extras/modules/prettify/prettify.css");
+
+model.viewJsDeps.push("extras/components/preview/web-preview-extend.js");
+model.viewJsDeps.push("extras/components/preview/Prettify.js");
+model.viewJsDeps.push("extras/modules/prettify/prettify.js");
