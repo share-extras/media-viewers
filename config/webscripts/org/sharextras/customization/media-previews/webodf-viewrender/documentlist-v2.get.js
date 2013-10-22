2d0e40173c379e616aa5260286aa047553168c9f
Peter Lofgren <peter@loftux.se>
2013-10-22 15:12:05 +0200

    #22 Filmstrip view - Added support for WebODF

---
+++ config/webscripts/org/sharextras/customization/media-previews/webodf-viewrender/documentlist-v2.get.js
@@ -0,0 +1,6 @@
+// Add dependencies for documentlibrary view renders.
+model.viewCssDeps.push("extras/components/preview/WebODF.css");
+model.viewCssDeps.push("extras/modules/webodf/webodf.css");
+
+model.viewJsDeps.push("extras/components/preview/WebODF.js");
+model.viewJsDeps.push("extras/modules/webodf/webodf.js");
