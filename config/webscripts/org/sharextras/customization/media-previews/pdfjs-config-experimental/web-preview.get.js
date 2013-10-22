12f07945994373810f5eb1549d40dfa8e18242c7
Peter Lofgren <peter@loftux.se>
2013-10-22 15:17:29 +0200

    #1 Adding configuration support for experimental Progressive loading. - New attribute added: progressiveLoading with default false. - Added new configuration option to enable this by module config.

---
+++ config/webscripts/org/sharextras/customization/media-previews/pdfjs-config-experimental/web-preview.get.js
@@ -0,0 +1,47 @@
+if (model.widgets)
+{
+   for (var i = 0; i < model.widgets.length; i++)
+   {
+      var widget = model.widgets[i];
+      if (widget.id == "WebPreview")
+      {
+         var conditions = [];
+         // Insert new pluginCondition(s) at start of the chain
+         conditions.push({
+            attributes: {
+               mimeType: "application/pdf"
+            },
+            plugins: [{
+               name: "PdfJs",
+               attributes: {progressiveLoading: "true"}
+            },
+            {
+               name: "Embed",
+               attributes: {}
+            }]
+         });
+         conditions.push({
+            attributes: {
+               thumbnail: "pdf"
+            },
+            plugins: [{
+               name: "PdfJs",
+               attributes: { src: "pdf", progressiveLoading: "true" }
+            },
+            {
+               name: "Embed",
+               attributes: { src: "pdf" }
+            }]
+         });
+         var oldConditions = eval("(" + widget.options.pluginConditions + ")");
+         // Add the other conditions back in
+         for (var j = 0; j < oldConditions.length; j++)
+         {
+            conditions.push(oldConditions[j]);
+         }
+         // Override the original conditions
+         model.pluginConditions = jsonUtils.toJSONString(conditions);
+         widget.options.pluginConditions = model.pluginConditions;
+      }
+   }
+}
